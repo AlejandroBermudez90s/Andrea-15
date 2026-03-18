86c55246514089d68be3c9cf8acb8b53300519d8
AlejandroBermúdez90s <2060559@alu.murciaeduca.es>
2026-03-18 22:49:23 +0100

    actualizado 2

--- src/componentes/Cabecera/Cabecera.jsx
+++ src/componentes/Cabecera/Cabecera.jsx
@@ -3,14 +3,18 @@ import './Cabecera.css'
 const Cabecera = () => {
 
 	return (
-		<div>
-			<h1 className="andrea">Andrea</h1>
-			<h2 className="andrea2">XV Años</h2>
-
-			<a href="#detalles" className="btn btn-outline-light btn-sm mt-4 px-3 shadow-sm">
-				Ver detalles
-			</a>
-		</div>
+		<>
+			<div className="texto">
+				<h1 className="andrea">Andrea</h1>
+				
+			</div>
+			<div className='boton'>
+				<h2 className="andrea2">15 Años</h2>
+				<a href="#detalles" className="btn btn-outline-light btn-sm mt-4 px-3 shadow-sm">
+					Ver detalles
+				</a>
+			</div>
+		</>
 	)
 }
 
